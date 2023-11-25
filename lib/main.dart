e37ce2748569d80c9376055771ad6088c28def07
Niels <niels.minssen@etu.uca.fr>
2023-11-25 16:05:56 +0800

    only a world map

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,62 @@
-import 'package:countries_world_map/data/maps/world_map.dart';
+import 'random_map.dart';
+//import 'package:example/pages/supported_countries_map.dart';
 import 'package:flutter/material.dart';
-import 'package:countries_world_map/countries_world_map.dart';
 
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(SMapExampleApp());
 }
 
-class MyApp extends StatelessWidget {
+class SMapExampleApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+        title: 'Worldmap Example',
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity),
+        home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController controller;
+
+  @override
+  void initState() {
+    controller = TabController(length: 2, initialIndex: 0, vsync: this);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
-          title: const Text('Countries World Map Example'),
-        ),
-        body: Center(
-          child: SimpleMap(
-            // String of instructions to draw the map.
-            instructions:SMapWorld.instructions,
-            // Default color for all countries.
-            defaultColor: Colors.grey,
-
-            // Matching class to specify custom colors for each area.
-            colors: const SMapWorldColors(
-              uS: Colors.green,   // This makes USA green
-              cN: Colors.green,   // This makes China green
-              rU: Colors.green,   // This makes Russia green
-            ).toMap(),
-
-            // Callback details of what area is being touched, giving you the ID, name, and tap details
-            callback: (id, name, tapdetails) {
-               print(id); // Prints the ID of the country that was tapped
-            },
-          ),
-        ),
-      ),
-    );
+            title: const Text('Countries World Map',
+                style: TextStyle(color: Colors.blue)),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+           ),
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: TabBarView(
+              physics: const NeverScrollableScrollPhysics(),
+              controller: controller,
+              children: [
+                //SupportedCountriesMap(),
+                RandomWorldMapGenerator(),
+                // AfricaContinent()
+              ]),
+        ));
   }
 }
